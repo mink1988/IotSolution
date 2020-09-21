f8d57abb2445131801e56e698594194df217264e
Alexander Bohlin <alexander.bohlin@hotmail.com>
2020-09-21 16:31:52 +0200

    ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using SharedLibraries.Models;
+using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Services;
 
 namespace ConsoleApp
 {
     class Program
     {
+        private static readonly string _conn = "";
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("", TransportType.Mqtt);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
         }
     }
 }
