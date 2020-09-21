5826231b93a66dd3fd3bfa28e68fd4810752c5d4
Alexander Bohlin <alexander.bohlin@hotmail.com>
2020-09-21 17:25:41 +0200

    ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,11 +7,14 @@ namespace ConsoleApp
 {
     class Program
     {
-        private static readonly string _conn = "";
-        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("", TransportType.Mqtt);
+        private static readonly string _conn = "HostName=Alex-win20-iothub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=Vx5YH1x8bEsBL0+jMGLHvgL2eJIwHNgsk2HmUNl2xjU=";
+        
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
         static void Main(string[] args)
         {
             DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+
+            Console.ReadKey();
         }
     }
 }
